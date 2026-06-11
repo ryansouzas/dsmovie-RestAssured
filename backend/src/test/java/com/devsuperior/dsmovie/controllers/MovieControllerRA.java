8c0d35edf00336a8b9348d77e59bd646d9b4b5aa
Ryan Souza <ryanssebadelhe@gmail.com>
2026-06-11 14:33:50 -0300

    MovieControllerRA - insert tests

--- backend/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ backend/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -4,23 +4,51 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.json.JSONException;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MovieControllerRA {
+
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+    private String clientToken, adminToken,invalidToken;
 
     private Long existingMovieId, nonExistingMovieId;
     private String movieTitle;
 
+    private Map<String, Object> postMovieInstance;
+
     @BeforeEach
-    void setUp(){
+    void setUp() throws Exception {
         baseURI = "http://localhost:8080";
 
         movieTitle = "Venom";
 
         existingMovieId = 1L;
         nonExistingMovieId = 100L;
+
+        clientUsername = "alex@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+
+        clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto";
+
+
+        postMovieInstance = new HashMap<>();
+        postMovieInstance.put("title", "Test Movie");
+        postMovieInstance.put("score", 0.0);
+        postMovieInstance.put("count", 0);
+        postMovieInstance.put("image", "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
 
     }
 
@@ -67,14 +95,48 @@ public class MovieControllerRA {
 	}
 	
 	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+        postMovieInstance.put("title", "");
+
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .body(postMovieInstance)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+           .when()
+                .post("/movies")
+           .then()
+                .statusCode(422);
+
+
 	}
 	
 	@Test
 	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .body(postMovieInstance)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+           .when()
+                .post("/movies")
+           .then()
+                .statusCode(403);
 	}
 	
 	@Test
 	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+        given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + invalidToken)
+                .body(postMovieInstance)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+           .when()
+                .post("/movies")
+            .then()
+                .statusCode(401);
 	}
 }
